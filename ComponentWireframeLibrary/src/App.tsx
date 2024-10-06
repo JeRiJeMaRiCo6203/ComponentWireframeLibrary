ad9a8ca9efd585559d1a098747bb1e4b186ff911
Syaf <syafiq.syech@gmail.com>
2024-10-06 21:20:37 +0700

    Rapihin Stuff

--- ComponentWireframeLibrary/src/App.tsx
+++ ComponentWireframeLibrary/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 class="underline">HELLO WORLD!</h1>
+      <h1 className="underline">HELLO WORLD!</h1>
     </>
   )
 }
