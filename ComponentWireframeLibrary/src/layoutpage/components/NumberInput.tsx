e711823ad3a073a06f22261edcdd79f7bd92d105
Syaf <syafiq.syech@gmail.com>
2025-02-14 20:44:37 +0700

    numberinput update

--- ComponentWireframeLibrary/src/layoutpage/components/NumberInput.tsx
+++ ComponentWireframeLibrary/src/layoutpage/components/NumberInput.tsx
@@ -30,7 +30,7 @@ const NumberInput: React.FC<NumberInputProps> = ({ numberRange, changeData, rese
   return (
     <div className='w-full mt-2 flex items-center gap-2'>
       <button 
-        className='p-2 px-3 bg-[#f4f4f4] hover:bg-[#e7e7e7] border-2 border-[#f4f4f4] hover:border-[#e7e7e7] rounded-lg select-none'
+        className={`p-2 px-3 hover:bg-[#e7e7e7] border-2 border-[#f4f4f4] hover:border-[#e7e7e7] rounded-lg select-none` + (amount <= numberRange[0] ? `bg-[#f4f4f4]` : `bg-white`)}
         onClick={() => handleAmount(amount - 1)}
       >
         -
@@ -44,7 +44,7 @@ const NumberInput: React.FC<NumberInputProps> = ({ numberRange, changeData, rese
         id=""
       />
       <button 
-        className='p-2 px-3 bg-[#f4f4f4] hover:bg-[#e7e7e7] border-2 border-[#f4f4f4] hover:border-[#e7e7e7] rounded-lg select-none'
+        className={`p-2 px-3 bg-[#f4f4f4] hover:bg-[#e7e7e7] border-2 border-[#f4f4f4] hover:border-[#e7e7e7] rounded-lg select-none` + (amount >= numberRange[1] ? `bg-[#f4f4f4]` : `bg-white`)}
         onClick={() => handleAmount(amount + 1)}
       >
         +
